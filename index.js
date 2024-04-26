efe1ac9288b546adce5d503a19a7d0def443337c
Tamojit <tamosrk@gmail.com>
2024-04-26 15:44:27 +0530

    Wrote basic operation functiond

---
+++ index.js
@@ -0,0 +1,17 @@
+function add(a, b){
+    return a+b
+};
+
+function substract(){
+    return a-b
+};
+
+function multiply(array){
+    return array.reduce((total, item)=>{
+        return total * item
+    }, 1)
+};
+
+function divide(a, b){
+    retun a/b
+}
