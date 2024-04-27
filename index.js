216cfbf4065cd126791998e1697d7f26972fb90f
Tamojit <tamosrk@gmail.com>
2024-04-27 15:49:34 +0530

    Made the ui for the calculator

--- index.js
+++ index.js
@@ -2,16 +2,31 @@ function add(a, b){
     return a+b
 };
 
-function substract(){
+function substract(a, b){
     return a-b
 };
 
-function multiply(array){
-    return array.reduce((total, item)=>{
-        return total * item
-    }, 1)
+function multiply(a, b){
+    return a*b
 };
 
 function divide(a, b){
     return a/b
 }
+
+let firstNo = 0;
+let operator = "";
+let secondNo =0;
+
+function operate(firstNo, operator, secondNo){
+    if(operator === "+"){
+        add(firstNo, secondNo)
+    }else if(operator === "-"){
+        substract(firstNo, secondNo)
+    }else if(operator === "*"){
+        multiply(firstNo, secondNo)
+    }else if(operator === "/"){
+        divide(firstNo, secondNo)
+    }
+};
+
