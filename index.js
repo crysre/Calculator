ec9c81097811d4348bb06fd3fe21353f5c798864
Tamojit <tamosrk@gmail.com>
2024-04-29 22:04:55 +0530

    Made the calculator work for the first time

--- index.js
+++ index.js
@@ -16,26 +16,83 @@ function divide(a, b){
     return a/b
 }
 
-let firstNo = 0;
+let firstNo = [];
 let operator = "";
-let secondNo =0;
+let secondNo =[];
 
 function operate(firstNo, operator, secondNo){
     if(operator === "+"){
-        add(firstNo, secondNo)
+        return add(firstNo, secondNo)
     }else if(operator === "-"){
-        substract(firstNo, secondNo)
+        return substract(firstNo, secondNo)
     }else if(operator === "*"){
-        multiply(firstNo, secondNo)
+        return multiply(firstNo, secondNo)
     }else if(operator === "/"){
-        divide(firstNo, secondNo)
+        return divide(firstNo, secondNo)
     }
 };
 
-let buttons = document.querySelectorAll(".items")
-let display = document.querySelector(".display")
+let firstNumber = 0;
+let secondNumber = 0;
 
-buttons.onclick((event)=>{
+let buttons = document.querySelectorAll(".n");
+let opButtons = document.querySelectorAll(".o")
+let display = document.querySelector(".display")
+let opDisplay = document.querySelector(".opDisplay")
+let execute = document.querySelector(".execute")
+let allClear = document.querySelector(".aClear")
+
+
+function clickhandler1 (event){
     let clickedButton = event.target;
-    display.innerText = clickedButton.textContent;
+    firstNo.push(clickedButton.textContent)
+    firstNumber = parseInt(firstNo.join(""))
+    display.innerText = firstNumber
+}
+
+function clickhandler2 (event){
+    let clickedButton = event.target;
+    secondNo.push(clickedButton.textContent)
+    secondNumber = parseInt(secondNo.join(""))
+    display.innerText = secondNumber
+}
+
+buttons.forEach((button)=>{
+    button.addEventListener("click", clickhandler1)
+})
+
+opButtons.forEach((buttonOp)=>{
+    buttonOp.addEventListener("click", (event)=>{
+        let clickedOp = event.target;
+        operator = clickedOp.textContent;
+        opDisplay.innerText = clickedOp.textContent;
+        
+        buttons.forEach((button)=>{
+            button.removeEventListener("click", clickhandler1)
+        })
+
+        buttons.forEach((button)=>{
+            button.addEventListener("click", clickhandler2)
+        })
+        
+    })
+})
+
+execute.addEventListener("click", ()=>{
+    let result = operate(firstNumber, operator, secondNumber);
+    display.innerText = result;
+ 
+})
+
+
+allClear.addEventListener("click", ()=>{
+
+    firstNo = []
+    secondNo = []
+    firstNumber = 0
+    secondNumber = 0
+    operator = null
+
+    display.innerText = 0
+    opDisplay.innerText = ""
 })
